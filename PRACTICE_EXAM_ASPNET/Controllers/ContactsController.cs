5ce5f3eb9434a5a059f28e3101a4f3b0649923b1
Vndark5464 <nguyenhuulinh5464@gmail.com>
2023-07-18 11:20:29 +0700

    abc

--- PRACTICE_EXAM_ASPNET/Controllers/ContactsController.cs
+++ PRACTICE_EXAM_ASPNET/Controllers/ContactsController.cs
@@ -1,65 +1,200 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using PRACTICE_EXAM_ASPNET.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using PRACTICE_EXAM_ASPNET.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PRACTICE_EXAM_ASPNET.Entities;
 
 namespace PRACTICE_EXAM_ASPNET.Controllers
 {
-    public class ContactsController : Controller
+    public class ContactController : Controller
     {
-        private List<Contact> contacts = new List<Contact>(); // Lưu trữ tạm thời các liên hệ trong bộ nhớ
+        private readonly DataContext _context;
 
-        public ActionResult Index(string sortOrder, string searchString)
+        public ContactController(DataContext context)
         {
-            // Sắp xếp danh sách liên hệ
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            var sortedContacts = contacts.OrderBy(c => c.ContactName);
+            _context = context;
+        }
+
+        // GET: Contact
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        {
+            ViewData["NameSortParam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["CurrentFilter"] = searchString;
+
+            var contacts = from c in _context.Contacts
+                           select c;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                contacts = contacts.Where(c => c.ContactName.Contains(searchString));
+            }
+
             switch (sortOrder)
             {
                 case "name_desc":
-                    sortedContacts = contacts.OrderByDescending(c => c.ContactName);
+                    contacts = contacts.OrderByDescending(c => c.ContactName);
                     break;
                 default:
-                    sortedContacts = contacts.OrderBy(c => c.ContactName);
+                    contacts = contacts.OrderBy(c => c.ContactName);
                     break;
             }
 
-            // Tìm kiếm thông tin liên hệ
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                sortedContacts = sortedContacts.Where(c => c.ContactName.Contains(searchString));
-            }
-
-            return View(sortedContacts.ToList());
+            return View(await contacts.ToListAsync());
         }
 
-        public ActionResult Create()
+        // GET: Contact/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contact/Create
+        public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Contact/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Contact contact)
+        public async Task<IActionResult> Create([Bind("Id,ContactName,ContactNumber,GroupName,HireDate,Birthday")] Contact contact)
         {
-            // Kiểm tra trùng tên liên hệ
-            if (contacts.Any(c => c.ContactName == contact.ContactName))
+            if (ModelState.IsValid)
             {
-                ModelState.AddModelError("ContactName", "Tên liên hệ đã tồn tại.");
+                bool isContactNameDuplicate = _context.Contacts.Any(c => c.ContactName == contact.ContactName);
+                bool isContactNumberDuplicate = _context.Contacts.Any(c => c.ContactNumber == contact.ContactNumber);
+
+                if (isContactNameDuplicate)
+                {
+                    ModelState.AddModelError("ContactName", "Contact Name already exists.");
+                }
+
+                if (isContactNumberDuplicate)
+                {
+                    ModelState.AddModelError("ContactNumber", "Contact Number already exists.");
+                }
+
+                if (isContactNameDuplicate || isContactNumberDuplicate)
+                {
+                    return View(contact);
+                }
+
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contact/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contact/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ContactName,ContactNumber,GroupName,HireDate,Birthday")] Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                // Tạo một ID duy nhất cho liên hệ mới
-                contact.Id = contacts.Count + 1;
-                contacts.Add(contact);
-                return RedirectToAction("Index");
+                try
+                {
+                    _context.Update(contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactExists(contact.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contact);
+        }
+
+        // GET: Contact/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
             }
 
             return View(contact);
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Contacts == null)
+            {
+                return Problem("Entity set 'DataContext.Contacts'  is null.");
+            }
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                _context.Contacts.Remove(contact);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactExists(int id)
+        {
+            return (_context.Contacts?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ PRACTICE_EXAM_ASPNET/Entities/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Exam.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
